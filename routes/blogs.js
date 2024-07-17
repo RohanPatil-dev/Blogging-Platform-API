b5638ff25e50b3d7ffa8b6e9f9cc9e60ec7cf718
Rohan Patil <rohanpatil5122001@gmail.com>
2024-07-17 19:50:28 +0530

    multer code refactored successfully

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,22 +9,9 @@ const {authorization} = require("../Middleware/auth")
 
 const blogs = require("../model/blogs")
 
+const {upload} = require("../Multer/multer")
+
 const {addBlogs,getAllData, singleData, updateData, deleteData, renderBlog} = require("../controller/blogs")
-
-const multer = require("multer")
-const path = require("path")
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.resolve("./public/uploads/"))
-    },
-    filename: function (req, file, cb) {
-        const filename = `${Date.now()} - ${file.originalname}`
-        cb(null, filename)
-    }
-})
-
-const upload = multer({storage : storage})
 
 router.post('/addBlogs',authorization,upload.single("coverImage"),addBlogs)
 
